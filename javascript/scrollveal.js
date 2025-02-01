20d9fa0dc34182f55d026fe668bed69d5cd19b7a
Przekop <przekop.michal@gmail.com>
2025-02-01 17:50:42 +0000

    Updated layout

--- javascript/scrollveal.js
+++ javascript/scrollveal.js
@@ -2,20 +2,20 @@ const defaultProps = {
   easing: "cubic-bezier(0.5, 0, 0, 1)",
   distance: "30px",
   origin: "bottom",
-  duration: 1000,
+  duration: 100,
   desktop: true,
   mobile: true,
 };
 // Hero Section
 ScrollReveal().reveal(".hero-title", {
   ...defaultProps,
-  delay: 500,
+  delay: 100,
   origin: window.innerWidth > 768 ? "left" : "bottom",
 });
 ScrollReveal().reveal(".name", {
   ...defaultProps,
   distance: "0px",
-  delay: 700,
+  delay: 100,
   scale: 0,
 });
 ScrollReveal().reveal(".hero-cta", {
@@ -25,25 +25,25 @@ ScrollReveal().reveal(".hero-cta", {
 });
 ScrollReveal().reveal(".scroll-down-link", {
   ...defaultProps,
-  delay: 2000,
+  delay: 100,
   origin: "top",
 });
 
 /* About Section */
 ScrollReveal().reveal(".section-title", {
   ...defaultProps,
-  delay: 400,
+  delay: 100,
   distance: "0px",
 });
 ScrollReveal().reveal(".about-wrapper__image", {
   ...defaultProps,
-  delay: 600,
+  delay: 100,
   scale: 0.5,
 });
 
 ScrollReveal().reveal(".about-wrapper__info", {
   ...defaultProps,
-  delay: 700,
+  delay: 100,
   distance: "100px",
   origin: window.innerWidth > 768 ? "left" : "top",
 });
@@ -51,13 +51,13 @@ ScrollReveal().reveal(".about-wrapper__info", {
 /* Projects Section */
 ScrollReveal().reveal(".project-wrapper__text", {
   ...defaultProps,
-  delay: 600,
+  delay: 100,
   origin: window.innerWidth > 768 ? "left" : "bottom",
 });
 
 ScrollReveal().reveal(".project-wrapper__image", {
   ...defaultProps,
-  delay: 600,
+  delay: 100,
   scale: 0.9,
   origin: window.innerWidth > 768 ? "right" : "bottom",
 });
@@ -65,11 +65,11 @@ ScrollReveal().reveal(".project-wrapper__image", {
 /* Contact Section */
 ScrollReveal().reveal(".contact-wrapper", {
   ...defaultProps,
-  delay: 600,
+  delay: 100,
 });
 
 ScrollReveal().reveal(".fa", {
   ...defaultProps,
-  delay: 500,
+  delay: 100,
   interval: 100,
 });
